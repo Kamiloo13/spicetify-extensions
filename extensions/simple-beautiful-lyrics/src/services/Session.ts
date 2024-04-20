e04ea6cd0d2c798cd41b6e18a47169a7aadf8be9
Kamil Hibszer <79232750+Kamiloo13@users.noreply.github.com>
2024-04-20 07:30:58 +0200

    Fix script.src check in Session.ts

--- extensions/simple-beautiful-lyrics/src/services/Session.ts
+++ extensions/simple-beautiful-lyrics/src/services/Session.ts
@@ -11,7 +11,7 @@ let IsDevelopment = false;
 
     for (const script of document.getElementsByTagName("script")) {
         if (script.src.includes("beautiful-lyrics.js")) {
-            if (script.src.includes("https://xpui.app.spotify.com/")) {
+            if (script.src.startsWith("https://xpui.app.spotify.com/")) {
                 if (developmentScript === undefined) {
                     developmentScript = script;
                 } else {
